26ae075bd4e91f9d429c8426dea1014ce3158003
Fanny Tan <fannyc310@gmail.com>
2023-12-31 01:58:29 -0600

    testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,3 @@
-// const licenseBadges = [
-//   { licenseName: 'Apache 2.0', badge: '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' },
-//   { licenseName: 'MIT', badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' },
-//   { licenseName: 'Unlicense', badge: '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)' }
-// ]
-
 const licenseBadges = [
   ['Apache 2.0', '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'],
   ['MIT', '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'],
@@ -21,25 +15,6 @@ function renderLicenseBadge(license) {
   }
 
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  // const renderBadge = licenseBadges.map(licenseBadge => {
-  //   if (license === licenseBadge.licenseName) {
-  //     return licenseBadge.badge;
-  //   } else {
-  //     return ""
-  //   }
-  // })
-
-  // ${renderLicenseSection(data.license)}
-};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
